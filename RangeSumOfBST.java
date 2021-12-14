3e6f3b6fe25643df23ef1fd6a3a9240060c9b7b8
zezomido78 <48326712+zezomido78@users.noreply.github.com>
2021-12-14 16:22:51 +0200

    14/12/2021

---
+++ RangeSumOfBST.java
@@ -0,0 +1,27 @@
+package solutions;
+
+import Datastructure.TreeNode;
+
+public class RangeSumOfBST {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int ans = 0;
+        if(root == null)
+            return 0;
+        if(inRange(root.val,low,high)){
+            ans += root.val;
+        }
+        if(root.left != null){
+            if(root.val >= low)
+                ans += rangeSumBST(root.left,low,high);
+        }
+        if(root.right != null){
+            if(root.val <= high)
+                ans += rangeSumBST(root.right,low,high);
+        }
+        return ans;
+    }
+    boolean inRange(int v,int l,int h){
+        return v>= l && v <= h;
+    }
+
+}
